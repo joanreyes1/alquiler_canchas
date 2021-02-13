0026e2f58694cba5339e47b2b0701b2984512d35
Gregory Arcentales <gregoarcenta@gmail.com>
2021-02-13 17:11:54 -0500

    se añadieron los archivos base

---
+++ src/alquiler_canchas/Alquiler_canchas.java
@@ -0,0 +1,13 @@
+
+package alquiler_canchas;
+
+import principal.Principal;
+
+public class Alquiler_canchas {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+    
+}
